f9a70f8d9b603c82dda28212dac4c88ca854c420
nawarazpokhrel <pokhrelnawaraz@yahoo.com>
2021-07-05 00:27:39 +0545

    Feat(notice):add migrations and serializers

---
+++ apps/notice/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+from apps.notice.models import Notice
+
+
+class NoticeSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Notice
+        fields = '__all__'
+
+
+class AddNoticeSerializers(NoticeSerializers):
+    class Meta(NoticeSerializers.Meta):
+        fields = (
+            'title',
+            'description',
+            'image',
+        )
+
+
+
