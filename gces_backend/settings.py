1a7ac8ad5e88c5383271cfaf46f64727c0fa42c4
nawarazpokhrel <pokhrelnawaraz@yahoo.com>
2021-07-04 15:00:55 +0545

    fix(proj):fix  settings cors

--- gces_backend/settings.py
+++ gces_backend/settings.py
@@ -29,14 +29,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CORS_ALLOWED_ORIGINS = [
-    "https://example.com",
-    "https://sub.example.com",
-    "http://localhost:3000",
-    "http://127.0.0.1:9000"
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "https://example.com",
+#     "https://sub.example.com",
+#     "http://localhost:3000",
+#     "http://127.0.0.1:9000"
+# ]
 
-# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = True
 # Application definition
 
 
