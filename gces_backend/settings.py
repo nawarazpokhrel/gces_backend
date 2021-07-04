28d72b72edfce1421764d215de6e17f455cf0407
nawarazpokhrel <pokhrelnawaraz@yahoo.com>
2021-07-04 14:53:21 +0545

    fix(proj):fix  middleware

--- gces_backend/settings.py
+++ gces_backend/settings.py
@@ -35,6 +35,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:9000"
 ]
+
+# CORS_ALLOW_ALL_ORIGINS = True
 # Application definition
 
 
@@ -62,6 +64,8 @@ THIRD_PARTY_APPS = [
 INSTALLED_APPS = DEFAULT_APPS + LOCAL_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
+
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -69,7 +73,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
 
